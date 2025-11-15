e892d074da480a96c5a5caa2fc6f811a0d4e3fca
Matius Robert <40347696+matrobert@users.noreply.github.com>
2025-11-15 15:11:18 +0000

    compress file size of background

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { BookOpen, PenTool, Download, Github } from "lucide-react";
 import { SiX, SiOrcid } from "react-icons/si";
-import dnaBackground from "@assets/Bird_DNA_1763217406416.png";
+import dnaBackground from "@assets/Bird_DNA_tiny.png";
 
 export default function Home() {
   const handleDownloadCV = () => {
